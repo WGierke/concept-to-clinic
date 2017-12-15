4149a799b68842ab93cb6f1b4913871d737c66b0
Willi Gierke <willi.gierke@student.hpi.de>
2017-12-15 23:54:03 +0100

    WIP

--- prediction/src/tests/test_classification.py
+++ prediction/src/tests/test_classification.py
@@ -1,17 +1,33 @@
+from os import path
+
+import pylidc as pl
+from config import Config
+from tqdm import tqdm
+
 from ..algorithms.classify import trained_model
 
 
-def test_classify_predict_load(metaimage_path, model_path):
-    assert not trained_model.predict(metaimage_path, [], model_path)
+def get_slice_number_from_z_value(scan, z_value):
+    images = scan.load_all_dicom_images()
+    start_z = images[0].ImagePositionPatient[-1]
+    return int((z_value - start_z) / scan.slice_thickness)
 
 
 def test_classify_dicom(dicom_paths, nodule_locations, model_path):
-    predicted = trained_model.predict(dicom_paths[0], nodule_locations, model_path)
-    assert predicted
-    assert 0 <= predicted[0]['p_concerning'] <= 1
+    scans = pl.query(pl.Scan).all()
+    MALICIOUS = 5
+    n = 0
+    error = 0
 
-
-def test_classify_luna(metaimage_path, luna_nodule, model_path):
-    predicted = trained_model.predict(metaimage_path, [luna_nodule], model_path)
-    assert predicted
-    assert 0 <= predicted[0]['p_concerning'] <= 1
+    for scan in tqdm(scans[:1]):
+        for a in scan.annotations:
+            if a.malignancy == MALICIOUS:
+                dir_ = '{}/{}/{}'.format(path.join(Config.FULL_DICOM_PATHS, scan.patient_id), scan.study_instance_uid,
+                                         scan.series_instance_uid)
+                centroid = list(a.centroid())
+                centroid[-1] = get_slice_number_from_z_value(scan, centroid[-1])
+                centroid_dict = {'x': centroid[0], 'y': centroid[1], 'z': centroid[2]}
+                predicted = trained_model.predict(dir_, [centroid_dict], model_path)
+                error += (predicted - 1) ** 2
+                n += 1
+        print(error / n)
